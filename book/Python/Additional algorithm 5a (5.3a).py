f3958e864a43fc1f54528c6e709ddb9a07166278
Paul A. Gagniuc <paul_gagniuc@acad.ro>
2023-04-06 14:34:05 +0300

    Add files via upload

---
+++ book/Python/Additional algorithm 5a (5.3a).py
@@ -0,0 +1,10 @@
+#Paul A. Gagniuc. An Introduction to Programming Languages: Simultaneous Learning in Multiple Coding Environments. Synthesis Lectures on Computer Science. Springer International Publishing, 2023, pp. 1-280.
+#Additional algorithm 5. It demonstrates multiple statements made on one line, and a line continuation for long statements. The statements shown here are very short, but the point of the exercise remains valid. Note that the source code is out of context and is intended for explanation of the method.
+
+# Recommended:
+
+a = "this is "
+b = a + "Python\n" + \
+"output"
+
+print (b)
