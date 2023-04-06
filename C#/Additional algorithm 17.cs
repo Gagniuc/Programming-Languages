bd7a6c291b46c2cd339a5bd1106a4a621861923e
Paul A. Gagniuc <paul_gagniuc@acad.ro>
2023-04-06 08:10:26 +0300

    Add files via upload

---
+++ C#/Additional algorithm 17.cs
@@ -0,0 +1,14 @@
+//Paul A. Gagniuc. An Introduction to Programming Languages: Simultaneous Learning in Multiple Coding Environments. Synthesis Lectures on Computer Science. Springer International Publishing, 2023, pp. 1-280.
+//Additional algorithm 17. It shows how to get the total number of elements of an array. First an array literal A is declared, that contains three elements, each with a string literal (one character). Next, a variable x is declared and a value is assigned to it. The value in question represents the number of elements in array A and is provided either by an in-built function or by a method of the array object, depending on the computer language used. Finally, the content of variable x is displayed in the output for inspection. One thing to note is that in VB, the ubound internal function returns the last index in the array and not the total number of elements as expected from the other examples. Note that the source code is in context and works with copy/paste.
+
+using System;
+class HelloWorld {
+  static void Main() {
+    
+    string[] A = {"a", "b", "c"};
+
+    int x = A.Length;
+
+    Console.WriteLine(x);
+  }
+}
---
+++ C#/Additional algorithm 31.cs
@@ -0,0 +1,18 @@
+//Paul A. Gagniuc. An Introduction to Programming Languages: Simultaneous Learning in Multiple Coding Environments. Synthesis Lectures on Computer Science. Springer International Publishing, 2023, pp. 1-280.
+//Additional algorithm 31. It shows the principle of nested function calls in which the return value of the most inner function becomes the argument for the most immediate outer function call, and so on. An integer literal is assigned to variable a. Then, the final return value of a group of nested function calls is assigned to a variable b, which in turn is printed to the output for inspection. Initially, the value stored in variable b is a negative value (ie -756029). Thus, for demonstration purposes, the minus sign is inserted in front of variable b in order to change the sign of the stored integer value (ie. b = -b). Note that the source code is in context and works with copy/paste.
+
+using System;
+class HelloWorld {
+  static void Main() {
+ 
+    int a = 3;
+    int b = c(c(c(c(a))));
+    b = -b;
+    
+    Console.WriteLine(b);
+  }
+  
+  static int c(int x)
+  {return x + x / x - x * x;}
+  
+}
