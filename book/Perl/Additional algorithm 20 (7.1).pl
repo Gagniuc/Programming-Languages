ae3dd4776fde6d21b1eb809ee95fc9991c42b326
Paul A. Gagniuc <paul_gagniuc@acad.ro>
2023-04-06 14:31:09 +0300

    Add files via upload

---
+++ book/Perl/Additional algorithm 20 (7.1).pl
@@ -0,0 +1,10 @@
+#Paul A. Gagniuc. An Introduction to Programming Languages: Simultaneous Learning in Multiple Coding Environments. Synthesis Lectures on Computer Science. Springer International Publishing, 2023, pp. 1-280.
+#Additional algorithm 20. Demonstrates the implementation of conditional statements. Three variables a, b and c are declared and assigned to different values. A condition triggers a statement to increment the value of variable c, only if the value of variable a is less than the value of variable b, otherwise a decrement is applied to the value of c. Note that the source code is in context and works with copy/paste.
+
+my $a = 1;
+my $b = 2;
+my $c = 3;
+
+if($a < $b){$c += 1;} else {$c -= 1;}
+
+print "$/c=" . $c;
---
+++ book/Perl/Additional algorithm 9 (6.3).pl
@@ -0,0 +1,16 @@
+#Paul A. Gagniuc. An Introduction to Programming Languages: Simultaneous Learning in Multiple Coding Environments. Synthesis Lectures on Computer Science. Springer International Publishing, 2023, pp. 1-280.
+#Additional algorithm 9. It shows explicit and implicit declarations of variables as well as examples of expressions and their evaluations for all computer languages used here. It mainly shows the connection between operators and data types. Note that the source code is in context and works with copy/paste.
+
+my $x = 12;
+my $y = 15;
+my $color = "green";
+my $xname = "Paul";
+my $logic = true;
+
+print ($logic);
+print ($x + $y);
+print ($color . $x);
+print ($x . $xname);
+print ($x + $y . $color);
+print ($color . $x + $y);
+print ($x + $x / $x - $x * $x);
