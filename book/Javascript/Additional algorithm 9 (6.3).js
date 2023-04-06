82ff5e4e3079da8080bc210a56b708f1b7d17d43
Paul A. Gagniuc <paul_gagniuc@acad.ro>
2023-04-06 17:04:43 +0300

    Update Additional algorithm 9 (6.3).js

--- book/Javascript/Additional algorithm 9 (6.3).js
+++ book/Javascript/Additional algorithm 9 (6.3).js
@@ -1,5 +1,5 @@
 //Paul A. Gagniuc. An Introduction to Programming Languages: Simultaneous Learning in Multiple Coding Environments. Synthesis Lectures on Computer Science. Springer International Publishing, 2023, pp. 1-280.
-//Additional algorithm 9. It shows explicit and implicit declarations of variables as well as examples of expressions and their evaluations for all computer languages used here. It mainly shows the connection between operators and data types. Note that the source code is in context and works with copy/paste.
+//Additional algorithm 6.3. It shows explicit and implicit declarations of variables as well as examples of expressions and their evaluations for all computer languages used here. It mainly shows the connection between operators and data types. Note that the source code is in context and works with copy/paste.
 //These JavaScript examples are written for the Rhino JavaScript engine. To use this particular Javascript example in browsers, please put the source between the <script></script> tags, and replace any "print" keyword with the "alert" keyword.
 
 var x = 12;
