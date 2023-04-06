d98bf8cbe7f7b1e00a1efbfe77347d9dab61b3af
Paul A. Gagniuc <paul_gagniuc@acad.ro>
2023-04-06 14:12:59 +0300

    Update Additional algorithm 8.php

--- PHP/Additional algorithm 8.php
+++ PHP/Additional algorithm 8.php
@@ -1,7 +1,7 @@
 <?php
 
 	//Paul A. Gagniuc. An Introduction to Programming Languages: Simultaneous Learning in Multiple Coding Environments. Synthesis Lectures on Computer Science. Springer International Publishing, 2023, pp. 1-280.
-    //Additional algorithm 8. It shows how values (literals) of different data types are assigned to variables. Please note that C#, Java and VB use the data type explicitly, i.e. the type of the variable is declared before assignment. On the other hand, notice that all the other environments use implicit data type, that is, the value is able to explicitly declare the variable type. Judging by the trends, it is possible that in the future explicit assignments may be less frequent. Note that the source code is in context and works with copy/paste.
+	//Additional algorithm 8. It shows how values (literals) of different data types are assigned to variables. Please note that C#, Java and VB use the data type explicitly, i.e. the type of the variable is declared before assignment. On the other hand, notice that all the other environments use implicit data type, that is, the value is able to explicitly declare the variable type. Judging by the trends, it is possible that in the future explicit assignments may be less frequent. Note that the source code is in context and works with copy/paste.
     
     my $x = 12;
     my $y = 15;
