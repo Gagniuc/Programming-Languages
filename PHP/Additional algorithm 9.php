375dcf48b88d2fb110b98f9e947e3dda7fc70811
Paul A. Gagniuc <paul_gagniuc@acad.ro>
2023-04-06 13:56:33 +0300

    Update Additional algorithm 9.php

--- PHP/Additional algorithm 9.php
+++ PHP/Additional algorithm 9.php
@@ -1,7 +1,7 @@
 <?php
 
 	//Paul A. Gagniuc. An Introduction to Programming Languages: Simultaneous Learning in Multiple Coding Environments. Synthesis Lectures on Computer Science. Springer International Publishing, 2023, pp. 1-280.
-    //Additional algorithm 9. It shows explicit and implicit declarations of variables as well as examples of expressions and their evaluations for all computer languages used here. It mainly shows the connection between operators and data types. Note that the source code is in context and works with copy/paste.
+	//Additional algorithm 9. It shows explicit and implicit declarations of variables as well as examples of expressions and their evaluations for all computer languages used here. It mainly shows the connection between operators and data types. Note that the source code is in context and works with copy/paste.
 
     $x = 12;
     $y = 15;
