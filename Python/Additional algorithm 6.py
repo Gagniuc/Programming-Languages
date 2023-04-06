e4978641d3537f0a32fdfbcbcf093baa36db3d42
Paul A. Gagniuc <paul_gagniuc@acad.ro>
2023-04-06 08:15:44 +0300

    Add files via upload

---
+++ Python/Additional algorithm 6.py
@@ -0,0 +1,18 @@
+#Paul A. Gagniuc. An Introduction to Programming Languages: Simultaneous Learning in Multiple Coding Environments. Synthesis Lectures on Computer Science. Springer International Publishing, 2023, pp. 1-280.
+#Additional algorithm 6. Examples of multiline comments are presented in the case of Python, which show the connection between the one-dimensional patterns from the previous table and the two-dimensional representation from the source code. Note that the source code is in context and works with copy/paste.
+
+print('''
+===========================================
+||                                       ||
+|| Python                                ||
+|| multiline comment [single commas]     ||
+===========================================
+''')
+
+print("""
+===========================================
+||                                       ||
+|| Python                                ||
+|| multiline comment [double commas]     ||
+===========================================
+""")
