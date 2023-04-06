148245ce0ffec1f36f2bc36a502e59d3c026da9d
Paul A. Gagniuc <paul_gagniuc@acad.ro>
2023-04-06 19:53:38 +0300

    Update Additional algorithm 29 (8.1).php

--- book/PHP/Additional algorithm 29 (8.1).php
+++ book/PHP/Additional algorithm 29 (8.1).php
@@ -1,7 +1,7 @@
 <?php
 
 	//Paul A. Gagniuc. An Introduction to Programming Languages: Simultaneous Learning in Multiple Coding Environments. Synthesis Lectures on Computer Science. Springer International Publishing, 2023, pp. 1-280.
-    //Additional algorithm 29. It shows the use of functions that take simple arguments. An integer literal is assigned to a variable a. Variable a is then used as an argument for a function called "compute". Function "compute" takes the argument and uses its value in a mathematical expression. The returned value of function "compute" is then assigned to a variable b, which is then printed into the output for inspection. Note that the source code is in context and works with copy/paste.
+	//Additional algorithm 8.1. It shows the use of functions that take simple arguments. An integer literal is assigned to a variable a. Variable a is then used as an argument for a function called "compute". Function "compute" takes the argument and uses its value in a mathematical expression. The returned value of function "compute" is then assigned to a variable b, which is then printed into the output for inspection. Note that the source code is in context and works with copy/paste.
     
     $a = 10;
     $b = compute($a);
