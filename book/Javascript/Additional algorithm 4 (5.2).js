d7a8c7148f9a7f469c50b459bf6502bbe2f8ccee
Paul A. Gagniuc <paul_gagniuc@acad.ro>
2023-04-06 17:02:56 +0300

    Update Additional algorithm 4 (5.2).js

--- book/Javascript/Additional algorithm 4 (5.2).js
+++ book/Javascript/Additional algorithm 4 (5.2).js
@@ -1,5 +1,5 @@
 //Paul A. Gagniuc. An Introduction to Programming Languages: Simultaneous Learning in Multiple Coding Environments. Synthesis Lectures on Computer Science. Springer International Publishing, 2023, pp. 1-280.
-//Additional algorithm 4. It shows basic good practices in JavaScript, such as: what is recommended, acceptable, and wrong. Note that the source code is out of context and is intended for explanation of the method.
+//Additional algorithm 5.2. It shows basic good practices in JavaScript, such as: what is recommended, acceptable, and wrong. Note that the source code is out of context and is intended for explanation of the method.
 //These JavaScript examples are written for the Rhino JavaScript engine. To use this particular Javascript example in browsers, please put the source between the <script></script> tags, and replace any "print" keyword with the "alert" keyword.
 
 //Recommended:
