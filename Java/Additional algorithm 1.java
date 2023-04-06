d712f686c62bd7fdedd982332ffc9e73b81e3332
Paul A. Gagniuc <paul_gagniuc@acad.ro>
2023-04-06 08:12:18 +0300

    Add files via upload

---
+++ Java/Additional algorithm 1.java
@@ -0,0 +1,9 @@
+//Paul A. Gagniuc. An Introduction to Programming Languages: Simultaneous Learning in Multiple Coding Environments. Synthesis Lectures on Computer Science. Springer International Publishing, 2023, pp. 1-280.
+//Additional algorithm 1. It shows the “Hello world” example for all computer languages used in this work. This is intended as a positive first introduction. Note that the source code is in context and works with copy/paste.
+
+public class Main
+{
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ Java/Additional algorithm 9.java
@@ -0,0 +1,23 @@
+//Paul A. Gagniuc. An Introduction to Programming Languages: Simultaneous Learning in Multiple Coding Environments. Synthesis Lectures on Computer Science. Springer International Publishing, 2023, pp. 1-280.
+//Additional algorithm 9. It shows explicit and implicit declarations of variables as well as examples of expressions and their evaluations for all computer languages used here. It mainly shows the connection between operators and data types. Note that the source code is in context and works with copy/paste.
+
+public class Main
+{
+    public static void main(String[] args) {
+        
+        int x = 12;
+        int y = 15;
+        String color = "green";
+        String xname = "Paul";
+        boolean logic = true;
+        
+        System.out.println(logic);
+        System.out.println(x + y);
+        System.out.println(color + x);
+        System.out.println(x + xname);
+        System.out.println(x + y + color);
+        System.out.println(color + x + y);
+        System.out.println(x + x / x - x * x);
+        
+    }
+}
