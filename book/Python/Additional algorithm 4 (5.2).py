16a882270c3965fbc57ff42445201f01d3d2bffc
Paul A. Gagniuc <paul_gagniuc@acad.ro>
2023-04-06 23:00:17 +0300

    Update Additional algorithm 4 (5.2).py

--- book/Python/Additional algorithm 4 (5.2).py
+++ book/Python/Additional algorithm 4 (5.2).py
@@ -1,5 +1,5 @@
 #Paul A. Gagniuc. An Introduction to Programming Languages: Simultaneous Learning in Multiple Coding Environments. Synthesis Lectures on Computer Science. Springer International Publishing, 2023, pp. 1-280.
-#Additional algorithm 4. It shows basic good practices in JavaScript, such as: what is recommended, acceptable, and wrong. Note that the source code is out of context and is intended for explanation of the method.
+#Additional algorithm 5.2. It shows basic good practices in JavaScript, such as: what is recommended, acceptable, and wrong. Note that the source code is out of context and is intended for explanation of the method.
 
 #Recommended:
 a = 7;
