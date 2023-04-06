b132b0151c409f01bcb2070885b84e333b813124
Paul A. Gagniuc <paul_gagniuc@acad.ro>
2023-04-06 14:30:04 +0300

    Add files via upload

---
+++ book/Javascript/Additional algorithm 1 (3.1).js
@@ -0,0 +1,7 @@
+//Paul A. Gagniuc. An Introduction to Programming Languages: Simultaneous Learning in Multiple Coding Environments. Synthesis Lectures on Computer Science. Springer International Publishing, 2023, pp. 1-280.
+//Additional algorithm 1. It shows the “Hello world” example for all computer languages used in this work. This is intended as a positive first introduction. Note that the source code is in context and works with copy/paste.
+//These JavaScript examples are written for the Rhino JavaScript engine. To use this particular Javascript example in browsers, please put the source between the <script></script> tags, and replace any "print" keyword with the "alert" keyword.
+
+<script>
+    alert("Hello World");
+</script>
