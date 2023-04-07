8676875ecc507ee06e93ecc05c36d7594a3872fa
Paul A. Gagniuc <paul_gagniuc@acad.ro>
2023-04-07 17:02:06 +0300

    Add files via upload

---
+++ by_chapter/Ch5 - Data Types and Statements/C++/Additional algorithm 3 (5.1).cpp
@@ -0,0 +1,13 @@
+//Paul A. Gagniuc. An Introduction to Programming Languages: Simultaneous Learning in Multiple Coding Environments. Synthesis Lectures on Computer Science. Springer International Publishing, 2023, pp. 1-280.
+//Additional algorithm 5.1. The first line of each computer language in the above list, shows an extraction of an ASCII character on the basis of an ASCII code. The second line shows the extraction of the ASCII code based on a given ASCII character. The output for any of the above statements is "Code 65 is the:'A' letter" and "Letter A has the code:'65'". Note that the source code is in context and works with copy/paste.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"Code 65 is the:'"<<(char) 65<<"' letter\n";
+    cout<<"Letter A has the code:'"<<(int) 'A'<<"'";
+
+    return 0;
+}
