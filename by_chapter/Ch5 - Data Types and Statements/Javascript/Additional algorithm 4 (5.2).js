a329026b6ac0c49e021c7e8d06ea3227edbde13d
Paul A. Gagniuc <paul_gagniuc@acad.ro>
2023-04-07 17:02:58 +0300

    Add files via upload

---
+++ by_chapter/Ch5 - Data Types and Statements/Javascript/Additional algorithm 4 (5.2).js
@@ -0,0 +1,15 @@
+//Paul A. Gagniuc. An Introduction to Programming Languages: Simultaneous Learning in Multiple Coding Environments. Synthesis Lectures on Computer Science. Springer International Publishing, 2023, pp. 1-280.
+//Additional algorithm 5.2. It shows basic good practices in JavaScript, such as: what is recommended, acceptable, and wrong. Note that the source code is out of context and is intended for explanation of the method.
+//These JavaScript examples are written for the Rhino JavaScript engine. To use this particular Javascript example in browsers, please put the source between the <script></script> tags, and replace any "print" keyword with the "alert" keyword.
+
+//Recommended:
+a = 7;
+b = 3;
+
+//Acceptable:
+a = 7; b = 3;
+
+//Wrong:
+3 = b;
+a =
+7;
