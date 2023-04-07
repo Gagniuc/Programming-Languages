5f04f9d7aecd5916c88b1872d8a240407bcb024c
Paul A. Gagniuc <paul_gagniuc@acad.ro>
2023-04-07 17:20:32 +0300

    Add files via upload

---
+++ by_chapter/Ch6 - Classic and Modern Variables/C++/Additional algorithm 13 (6.7).cpp
@@ -0,0 +1,16 @@
+//Paul A. Gagniuc. An Introduction to Programming Languages: Simultaneous Learning in Multiple Coding Environments. Synthesis Lectures on Computer Science. Springer International Publishing, 2023, pp. 1-280.
+//Additional algorithm 6.7. It shows how to create a multi-valued one-dimensional array variable using literals. In this example an array variable A is used to store only string literals and an array variable B is used to store integer literals. In languages such as Javascript, PHP, PERL, Ruby or Python, array variables can store several types of literals, including objects. In languages such as Java, C# and VB, array variables can store only one type of literal. Note that the source code is in context and works with copy/paste.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string A[] = {"a", "b", "c"};
+    int B[] = {1, 2, 3};
+    
+    cout<<(A[0] + A[1] + A[2]);
+    cout<<(B[0] + B[1] + B[2]);
+
+    return 0;
+}
---
+++ by_chapter/Ch6 - Classic and Modern Variables/C++/Additional algorithm 8 (6.2).cpp
@@ -0,0 +1,14 @@
+//Paul A. Gagniuc. An Introduction to Programming Languages: Simultaneous Learning in Multiple Coding Environments. Synthesis Lectures on Computer Science. Springer International Publishing, 2023, pp. 1-280.
+//Additional algorithm 6.2. It shows how values (literals) of different data types are assigned to variables. Please note that C#, Java and VB use the data type explicitly, i.e. the type of the variable is declared before assignment. On the other hand, notice that all the other environments use implicit data type, that is, the value is able to explicitly declare the variable type. Judging by the trends, it is possible that in the future explicit assignments may be less frequent. Note that the source code is in context and works with copy/paste.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x = 12;
+    int y = 15;
+    string color = "green";
+    string xname = "Paul";
+    bool logic = true;
+}
