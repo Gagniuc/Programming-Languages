d3cfbe1baee7b53b20de32061ff5fdd01b0faaeb
Paul A. Gagniuc <paul_gagniuc@acad.ro>
2023-04-07 17:31:51 +0300

    Add files via upload

---
+++ by_chapter/Ch7 - Control Structures/Ruby/Additional algorithm 22b (7.3b).rb
@@ -0,0 +1,12 @@
+#Paul A. Gagniuc. An Introduction to Programming Languages: Simultaneous Learning in Multiple Coding Environments. Synthesis Lectures on Computer Science. Springer International Publishing, 2023, pp. 1-280.
+#Additional algorithm 7.3b. Here the positive increment while-loop structure is demonstrated. A variable i is declared and set to zero. A while loop structure increments variable i from its initial value to its upper limit (number five). At each iteration, variable i is printed in the output. The result is an enumeration of values from 0 to 4. Note that the source code is in context and works with copy/paste.
+
+i = 0
+
+loop do
+    
+    puts "i = " + i.to_s
+    i += 1
+    
+    if i >= 5 then break end
+end
---
+++ by_chapter/Ch7 - Control Structures/Ruby/Additional algorithm 27 (7.8).rb
@@ -0,0 +1,30 @@
+#Paul A. Gagniuc. An Introduction to Programming Languages: Simultaneous Learning in Multiple Coding Environments. Synthesis Lectures on Computer Science. Springer International Publishing, 2023, pp. 1-280.
+#Additional algorithm 7.8. It demonstrates the use of a single for-loop for two-dimensional arrays. It shows the traversal of a two-dimensional array by one for-loop structure. A 2D-array variable (A) is declared with mixed datatypes as before, namely with string literals and number literals. A string variable t is initially set to empty. A variable v is set to zero and it represents the main counter of the for-loop. Another two variables (ie. i and j) are initialized with value zero and are the main coordinates for element identification. Each dimension of array A is stored in variables n and m, namely the number of rows in n and the number of columns in m. The upper limit of the for-loop is calculated based on the two known dimensions n and m. Thus, m times n establishes the upper limit of the for-loop. Here, the value of the counter v from the for-loop is used to calculate the i and j values that are used as an index to traverse the array variable A. The value of variable j is computed as the v % m and the result of this expression indicates the reminder (ex. 5 mod 3 is 2). The secret to this implementation is a condition that increments a variable i (rows) each time j (columns) equals zero. Thus, in this manner this approach provides the i and j values that a nested for-loop provides. At each iteration, the value from an element is added to the content of variable t. Once the end of the for-loop is reached, the value collected in variable t is printed in the output for inspection. The end result is the enumeration of each value in the output, in a linear manner. Note that the source code is in context and works with copy/paste.
+
+A = [
+    ["a", 88, 146],
+    ["b", 34, 124],
+    ["c", 96, 564],
+    [100, 12, "d"],
+    ]
+
+t = ""
+n = (A.size)     # rows
+m = (A[0].size)  # columns
+
+i = 0
+j = 0
+
+for v in 0..(n*m)-1
+
+    j = v % m
+
+    if(v != 0 and j == 0)
+        i = i + 1
+    end
+    
+    t += v.to_s + " A[" + i.to_s + "]["
+    t += j.to_s + "]=" + A[i][j].to_s + "\n"
+end
+
+puts(t)
