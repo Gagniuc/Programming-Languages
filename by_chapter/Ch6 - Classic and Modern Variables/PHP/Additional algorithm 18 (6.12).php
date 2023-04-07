c74ffef2cbf75b7461c6ef25529f341c9ce57919
Paul A. Gagniuc <paul_gagniuc@acad.ro>
2023-04-07 17:22:35 +0300

    Add files via upload

---
+++ by_chapter/Ch6 - Classic and Modern Variables/PHP/Additional algorithm 18 (6.12).php
@@ -0,0 +1,8 @@
+<?php
+
+	//Paul A. Gagniuc. An Introduction to Programming Languages: Simultaneous Learning in Multiple Coding Environments. Synthesis Lectures on Computer Science. Springer International Publishing, 2023, pp. 1-280.
+    //Additional algorithm 6.12. It presents nested arrays in Javascript, Ruby and Python. Three array variables A, B and C are declared here, each with three literal values. To represent the notion of nested, three other array variables are declared, namely D, E and F, each with three elements that hold one of the arrays A, B or C. To provide yet another level in the nest, a last three-element array variable is declared (ie. G), in which each element takes one of the recently mentioned arrays (ie. D, E or F). Note that the source code is in context and works with copy/paste.
+    
+    //this example was given for Javascript, Python and Ruby.
+
+?>
---
+++ by_chapter/Ch6 - Classic and Modern Variables/PHP/Additional algorithm 9 (6.3).php
@@ -0,0 +1,19 @@
+<?php
+
+	//Paul A. Gagniuc. An Introduction to Programming Languages: Simultaneous Learning in Multiple Coding Environments. Synthesis Lectures on Computer Science. Springer International Publishing, 2023, pp. 1-280.
+	//Additional algorithm 6.3. It shows explicit and implicit declarations of variables as well as examples of expressions and their evaluations for all computer languages used here. It mainly shows the connection between operators and data types. Note that the source code is in context and works with copy/paste.
+
+    $x = 12;
+    $y = 15;
+    $color = "green";
+    $xname = "Paul";
+    $logic = true;
+
+    print ($logic);
+    print ($x + $y);
+    print ($color . $x);
+    print ($x . $xname);
+    print ($x + $y . $color);
+    print $color . ($x + $y);
+    print ($x + $x / $x - $x * $x);
+?>
