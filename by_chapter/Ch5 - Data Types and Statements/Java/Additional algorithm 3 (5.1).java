f30e908b4887759f2b599f4c303432f372ab3a07
Paul A. Gagniuc <paul_gagniuc@acad.ro>
2023-04-07 17:02:29 +0300

    Add files via upload

---
+++ by_chapter/Ch5 - Data Types and Statements/Java/Additional algorithm 3 (5.1).java
@@ -0,0 +1,11 @@
+//Paul A. Gagniuc. An Introduction to Programming Languages: Simultaneous Learning in Multiple Coding Environments. Synthesis Lectures on Computer Science. Springer International Publishing, 2023, pp. 1-280.
+//Additional algorithm 5.1. The first line of each computer language in the above list, shows an extraction of an ASCII character on the basis of an ASCII code. The second line shows the extraction of the ASCII code based on a given ASCII character. The output for any of the above statements is "Code 65 is the:'A' letter" and "Letter A has the code:'65'". Note that the source code is in context and works with copy/paste.
+
+public class Main
+{
+	public static void main(String[] args) 
+	{
+        System.out.println("Code 65 is the:'" + (char) 65 + "' letter");
+        System.out.println("Letter A has the code:'" + (int) 'A' + "'");
+	}
+}
