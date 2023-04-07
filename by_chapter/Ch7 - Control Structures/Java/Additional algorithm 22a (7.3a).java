094f7b43b0ba7c97229e399e103056e47dc7bd5e
Paul A. Gagniuc <paul_gagniuc@acad.ro>
2023-04-07 17:28:42 +0300

    Add files via upload

---
+++ by_chapter/Ch7 - Control Structures/Java/Additional algorithm 22a (7.3a).java
@@ -0,0 +1,15 @@
+//Paul A. Gagniuc. An Introduction to Programming Languages: Simultaneous Learning in Multiple Coding Environments. Synthesis Lectures on Computer Science. Springer International Publishing, 2023, pp. 1-280.
+//Additional algorithm 7.3a. Here the positive increment while-loop structure is demonstrated. A variable i is declared and set to zero. A while loop structure increments variable i from its initial value to its upper limit (number five). At each iteration, variable i is printed in the output. The result is an enumeration of values from 0 to 4. Note that the source code is in context and works with copy/paste.
+
+public class Main
+{
+    public static void main(String[] args) 
+	{
+        int i = 0;
+        
+        while (i < 5) {
+          System.out.println("i = " + i);
+          i++;
+        }
+    }
+}
