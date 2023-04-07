969cb16466deb037a679e1fdb0972ca342a532d4
Paul A. Gagniuc <paul_gagniuc@acad.ro>
2023-04-07 17:05:09 +0300

    Add files via upload

---
+++ by_chapter/Ch5 - Data Types and Statements/Ruby/Additional algorithm 6 (5.4).rb
@@ -0,0 +1,4 @@
+#Paul A. Gagniuc. An Introduction to Programming Languages: Simultaneous Learning in Multiple Coding Environments. Synthesis Lectures on Computer Science. Springer International Publishing, 2023, pp. 1-280.
+#Additional algorithm 5.4. Examples of multiline comments are presented in the case of Python, which show the connection between the one-dimensional patterns from the previous table and the two-dimensional representation from the source code. Note that the source code is in context and works with copy/paste.
+
+#the example is only for python
