b0d954d5d1f00a09af677497af496c7ef66cc1fb
Paul A. Gagniuc <paul_gagniuc@acad.ro>
2023-04-07 17:21:13 +0300

    Add files via upload

---
+++ by_chapter/Ch6 - Classic and Modern Variables/Java/Additional algorithm 15 (6.9).java
@@ -0,0 +1,15 @@
+//Paul A. Gagniuc. An Introduction to Programming Languages: Simultaneous Learning in Multiple Coding Environments. Synthesis Lectures on Computer Science. Springer International Publishing, 2023, pp. 1-280.
+//Additional algorithm 6.8. It shows how to access the values stored in the elements of an array variable. An array literal is declared, in which three string values (three separate characters, namely "a", "b", "c") are stored. Then, two variables x and y are declared, which take values from the elements of the array variable A. Then, once assigned to the x and y variables, the string values are displayed in the output for visualization. As it can be observed, the result obtained after the execution is "bc". Note that the source code is in context and works with copy/paste.
+
+public class Main
+{
+    public static void main(String[] args) {
+        
+      String[] A = {"a", "b", "c"};
+      
+      String x = A[1];
+      String y = A[2];
+        
+      System.out.println(x + y);
+    }
+}
