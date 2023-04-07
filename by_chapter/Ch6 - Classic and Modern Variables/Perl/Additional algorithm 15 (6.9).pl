54b0316c3565d16909d0bfa8abaa0bde90cf4422
Paul A. Gagniuc <paul_gagniuc@acad.ro>
2023-04-07 17:23:06 +0300

    Add files via upload

---
+++ by_chapter/Ch6 - Classic and Modern Variables/Perl/Additional algorithm 15 (6.9).pl
@@ -0,0 +1,9 @@
+#Paul A. Gagniuc. An Introduction to Programming Languages: Simultaneous Learning in Multiple Coding Environments. Synthesis Lectures on Computer Science. Springer International Publishing, 2023, pp. 1-280.
+#Additional algorithm 6.9. It shows how to access the values stored in the elements of an array variable. An array literal is declared, in which three string values (three separate characters, namely "a", "b", "c") are stored. Then, two variables x and y are declared, which take values from the elements of the array variable A. Then, once assigned to the x and y variables, the string values are displayed in the output for visualization. As it can be observed, the result obtained after the execution is "bc". Note that the source code is in context and works with copy/paste.
+
+my @A = ("a", "b", "c");
+
+my $x = $A[1];
+my $y = $A[2];
+
+print($x . $y);
---
+++ by_chapter/Ch6 - Classic and Modern Variables/Perl/Additional algorithm 8 (6.2).pl
@@ -0,0 +1,8 @@
+#Paul A. Gagniuc. An Introduction to Programming Languages: Simultaneous Learning in Multiple Coding Environments. Synthesis Lectures on Computer Science. Springer International Publishing, 2023, pp. 1-280.
+#Additional algorithm 6.2. It shows how values (literals) of different data types are assigned to variables. Please note that C#, Java and VB use the data type explicitly, i.e. the type of the variable is declared before assignment. On the other hand, notice that all the other environments use implicit data type, that is, the value is able to explicitly declare the variable type. Judging by the trends, it is possible that in the future explicit assignments may be less frequent. Note that the source code is in context and works with copy/paste.
+
+my $x = 12;
+my $y = 15;
+my $color = "green";
+my $xname = "Paul";
+my $logic = true;
