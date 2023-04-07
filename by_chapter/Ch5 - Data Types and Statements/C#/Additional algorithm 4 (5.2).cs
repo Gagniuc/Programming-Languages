d3b6f5cfc4dd36e1986c66fb7025e67a52cbb82f
Paul A. Gagniuc <paul_gagniuc@acad.ro>
2023-04-07 17:01:37 +0300

    Add files via upload

---
+++ by_chapter/Ch5 - Data Types and Statements/C#/Additional algorithm 4 (5.2).cs
@@ -0,0 +1,23 @@
+//Paul A. Gagniuc. An Introduction to Programming Languages: Simultaneous Learning in Multiple Coding Environments. Synthesis Lectures on Computer Science. Springer International Publishing, 2023, pp. 1-280.
+//Additional algorithm 5.2. It shows basic good practices in JavaScript, such as: what is recommended, acceptable, and wrong. Note that the source code is out of context and is intended for explanation of the method.
+
+using System;
+class HelloWorld {
+  static void Main() { 
+	
+	int a, b, c;
+	
+	//Recommended:
+	a = 7;
+	b = 3;
+
+	//Acceptable:
+	a = 7; b = 3;
+
+	//Wrong:
+	//3 = b;
+	//a =
+	//7;
+
+  }
+}
---
+++ by_chapter/Ch5 - Data Types and Statements/C#/Additional algorithm 5b (5.3b).cs
@@ -0,0 +1,12 @@
+//Paul A. Gagniuc. An Introduction to Programming Languages: Simultaneous Learning in Multiple Coding Environments. Synthesis Lectures on Computer Science. Springer International Publishing, 2023, pp. 1-280.
+//Additional algorithm 5.3b. It demonstrates multiple statements made on one line, and a line continuation for long statements. The statements shown here are very short, but the point of the exercise remains valid. Note that the source code is out of context and is intended for explanation of the method.
+
+using System;
+class HelloWorld {
+  static void Main() {
+
+	// Acceptable:
+
+	String a = "this is "; String b = a + "C#\n" + "output"; Console.WriteLine(b);
+  }
+}
